f1100fabda503aa6810211fb06b5f7c126ba1bca
Raphael Matile <raphael.matile@gmail.com>
2015-12-04 11:29:15 +0100

    Add missing method

--- src/test/java/org/rmatil/sync/event/aggregator/test/mocks/ObjectManagerMock.java
+++ src/test/java/org/rmatil/sync/event/aggregator/test/mocks/ObjectManagerMock.java
@@ -7,7 +7,9 @@ import org.rmatil.sync.version.api.IObjectManager;
 import org.rmatil.sync.version.core.model.Index;
 import org.rmatil.sync.version.core.model.PathObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -42,6 +44,19 @@ public class ObjectManagerMock implements IObjectManager {
     public void removeObject(String fileNameHash)
             throws InputOutputException {
         this.pathObjects.remove(fileNameHash);
+    }
+
+    @Override
+    public List<PathObject> getChildren(String s)
+            throws InputOutputException {
+        List<PathObject> pathObjects = new ArrayList<>();
+        for (Map.Entry<String, PathObject> entry : this.pathObjects.entrySet()) {
+            if (entry.getKey().startsWith(s + "/")) {
+                pathObjects.add(entry.getValue());
+            }
+        }
+
+        return pathObjects;
     }
 
     @Override
