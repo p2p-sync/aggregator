13f56b027a735795262a0843b40290d34e3f1c8b
Raphael Matile <raphael.matile@gmail.com>
2016-01-12 19:15:14 +0100

    Add order for delete events

--- src/main/java/org/rmatil/sync/event/aggregator/core/modifier/AddDirectoryContentModifier.java
+++ src/main/java/org/rmatil/sync/event/aggregator/core/modifier/AddDirectoryContentModifier.java
@@ -47,7 +47,7 @@ public class AddDirectoryContentModifier implements IModifier {
                     modifiedEvents.addAll(
                             deletedChildren
                                     .stream()
-                                    .map(entry -> new DeleteEvent(Paths.get(entry.getAbsolutePath()), entry.getName(), null, event.getTimestamp()))
+                                    .map(entry -> new DeleteEvent(Paths.get(entry.getAbsolutePath()), entry.getName(), null, event.getTimestamp() - this.getAdditionalMilliseconds(event.getPath().toString(), entry.getAbsolutePath())))
                                     .collect(Collectors.toList()));
                 } catch (InputOutputException e) {
                     logger.error("Failed to get deleted objects from directory " + event.getPath().toString() + ". Message: " + e.getMessage());
@@ -75,18 +75,16 @@ public class AddDirectoryContentModifier implements IModifier {
 
         // traverse each file
         for (File file : parentDirectory.listFiles()) {
-            if (file.isFile()) {
-                try {
-                    logger.trace("Create createEvent for subfile " + file.toPath().toString() + " in parentDir " + parentDirectory.toString());
-                    // add additional n milliseconds such that the child contents are processed later than the parent ones
-                    events.add(new CreateEvent(this.rootDir.relativize(file.toPath()), file.getName(), Hash.hash(Config.DEFAULT.getHashingAlgorithm(), file), timestamp + this.getAdditionalMilliseconds(parentDirectory.toString(), file.toString())));
-                } catch (IOException e) {
-                    logger.error("Could not hash contents of file " + file.toPath().toString());
-                }
-            } else if (file.isDirectory()) {
-                // create createEvent for subdir
+            try {
+                logger.trace("Create createEvent for subfile " + file.toPath().toString() + " in parentDir " + parentDirectory.toString());
                 // add additional n milliseconds such that the child contents are processed later than the parent ones
-                events.add(new CreateEvent(this.rootDir.relativize(file.toPath()), file.getName(), null, timestamp + this.getAdditionalMilliseconds(parentDirectory.toString(), file.toString())));
+                events.add(new CreateEvent(this.rootDir.relativize(file.toPath()), file.getName(), Hash.hash(Config.DEFAULT.getHashingAlgorithm(), file), timestamp + this.getAdditionalMilliseconds(parentDirectory.toString(), file.toString())));
+            } catch (IOException e) {
+                logger.error("Could not hash contents of file " + file.toPath().toString());
+            }
+
+            if (file.isDirectory()) {
+                // add additional n milliseconds such that the child contents are processed later than the parent ones
                 events.addAll(this.createCreateEventForChildren(file, timestamp + this.getAdditionalMilliseconds(parentDirectory.toString(), file.toString())));
             }
         }
