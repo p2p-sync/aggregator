b08cb20f12d9437f16cff42e45a99802b6d69b74
Raphael Matile <raphael.matile@gmail.com>
2016-03-01 09:58:01 +0100

    p2p-sync/sync#35: Add support for file glob patterns

--- src/main/java/org/rmatil/sync/event/aggregator/core/modifier/IgnorePathsModifier.java
+++ src/main/java/org/rmatil/sync/event/aggregator/core/modifier/IgnorePathsModifier.java
@@ -4,7 +4,9 @@ import org.rmatil.sync.event.aggregator.core.events.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.nio.file.PathMatcher;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -17,13 +19,26 @@ public class IgnorePathsModifier implements IModifier {
 
     final static Logger logger = LoggerFactory.getLogger(IgnorePathsModifier.class);
 
-    protected List<Path> ignoredPaths;
+    protected Path         rootDir;
+    protected List<Path>   ignoredPaths;
+    protected List<String> ignoredPatterns;
 
     /**
      * @param ignoredPaths A list of paths (relative to the root of the sync folder) which are ignored
      */
-    public IgnorePathsModifier(List<Path> ignoredPaths) {
+    public IgnorePathsModifier(Path rootDir, List<Path> ignoredPaths) {
+        this(rootDir, ignoredPaths, new ArrayList<>());
+    }
+
+    /**
+     * @param rootDir         The root directory to which the paths should be resolved when using glob patterns
+     * @param ignoredPaths    A list of paths (relative to the root of the sync folder) which are ignored
+     * @param ignoredPatterns A list of regex patterns
+     */
+    public IgnorePathsModifier(Path rootDir, List<Path> ignoredPaths, List<String> ignoredPatterns) {
+        this.rootDir = rootDir;
         this.ignoredPaths = ignoredPaths;
+        this.ignoredPatterns = ignoredPatterns;
     }
 
     @Override
@@ -49,6 +64,30 @@ public class IgnorePathsModifier implements IModifier {
                         isChildOfIgnored = true;
                         logger.trace("Ignoring the move of file " + event.getPath().toString() + " to " + ((MoveEvent) event).getNewPath() + " since it is stored in the ignored path " + path.toString());
                         break;
+                    }
+                }
+            }
+
+            if (! isChildOfIgnored) {
+                // check if the path is matching any ignored pattern
+                for (String pattern : this.ignoredPatterns) {
+                    PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:" + pattern);
+                    // resolve paths relative to the root dir
+                    if (event instanceof CreateEvent ||
+                            event instanceof ModifyEvent ||
+                            event instanceof DeleteEvent) {
+                        if (matcher.matches(this.rootDir.relativize(event.getPath()))) {
+                            isChildOfIgnored = true;
+                            logger.trace("Ignoring file " + event.getPath().toString() + " since it matches the registered glob pattern " + pattern);
+                            break;
+                        }
+                    } else if (event instanceof MoveEvent) {
+                        if (matcher.matches(this.rootDir.relativize(event.getPath())) ||
+                                matcher.matches(this.rootDir.relativize(((MoveEvent) event).getNewPath()))) {
+                            isChildOfIgnored = true;
+                            logger.trace("Ignoring the move of file " + event.getPath().toString() + " to " + ((MoveEvent) event).getNewPath() + " since it matches the registered glob pattern " + pattern);
+                            break;
+                        }
                     }
                 }
             }
--- src/test/java/org/rmatil/sync/event/aggregator/test/core/modifier/IgnorePathsModifierTest.java
+++ src/test/java/org/rmatil/sync/event/aggregator/test/core/modifier/IgnorePathsModifierTest.java
@@ -17,6 +17,7 @@ import static org.junit.Assert.assertThat;
 public class IgnorePathsModifierTest {
 
     private static IgnorePathsModifier ignorePathsModifier;
+    private static IgnorePathsModifier ignorePathsModifier2;
 
     private static final Path ROOT_TEST_DIR = Paths.get("/tmp/dir/");
 
@@ -26,7 +27,16 @@ public class IgnorePathsModifierTest {
     public static void setUp() {
         List<Path> ignoredPaths = new ArrayList<>();
         ignoredPaths.add(IGNORED_DIR);
-        ignorePathsModifier = new IgnorePathsModifier(ignoredPaths);
+        ignorePathsModifier = new IgnorePathsModifier(ROOT_TEST_DIR, ignoredPaths);
+
+        List<String> ignoredPatterns = new ArrayList<>();
+        ignoredPatterns.add("*.java"); // ignore all files ending with .java
+        ignoredPatterns.add("**_*"); // ignore all files having a underline in the name
+        ignoredPatterns.add(".DS_Store"); // ignore all .DS_Store files
+        ignoredPatterns.add("**.swx"); // ignore swap files of vim (?)
+        ignoredPatterns.add("**.swp"); // ignore swap files of vim
+
+        ignorePathsModifier2 = new IgnorePathsModifier(ROOT_TEST_DIR, ignoredPaths, ignoredPatterns);
     }
 
     @Test
@@ -48,9 +58,46 @@ public class IgnorePathsModifierTest {
 
         List<IEvent> modifiedEvents = ignorePathsModifier.modify(eventList);
 
-        assertEquals("to less events are ignored", 2,  modifiedEvents.size());
+        assertEquals("to less events are ignored", 2, modifiedEvents.size());
 
         assertThat("First event is delete event", modifiedEvents.get(0), instanceOf(DeleteEvent.class));
         assertThat("2nd event is move event", modifiedEvents.get(1), instanceOf(MoveEvent.class));
+    }
+
+    @Test
+    public void testWithIgnoreGlobs() {
+        IEvent ev1 = new CreateEvent(ROOT_TEST_DIR.resolve(".sync/someFile.txt"), "someFile.txt", "someHash", System.currentTimeMillis());
+        IEvent ev2 = new ModifyEvent(ROOT_TEST_DIR.resolve(".sync/someDir/someFile.txt"), "someFile.txt", "someHash", System.currentTimeMillis());
+        IEvent ev3 = new DeleteEvent(ROOT_TEST_DIR.resolve("/tmp/dir/someFile.txt"), "someFile.txt", "someHash", System.currentTimeMillis());
+        IEvent ev4 = new MoveEvent(ROOT_TEST_DIR.resolve("/tmp/dir/someFile.txt"), ROOT_TEST_DIR.resolve("/tmp/dir/newFile.txt"), "newFile.txt", "someHash", System.currentTimeMillis());
+        IEvent ev5 = new MoveEvent(ROOT_TEST_DIR.resolve(".sync/someFile.txt"), ROOT_TEST_DIR.resolve("/tmp/dir/newFile.txt"), "newFile.txt", "someHash", System.currentTimeMillis());
+        IEvent ev6 = new MoveEvent(ROOT_TEST_DIR.resolve("/tmp/dir/someFile.txt"), ROOT_TEST_DIR.resolve(".sync/newFile.txt"), "newFile.txt", "someHash", System.currentTimeMillis());
+        IEvent ev7 = new CreateEvent(ROOT_TEST_DIR.resolve(".DS_Store"), ".DS_Store", "someHash", System.currentTimeMillis());
+        IEvent ev8 = new CreateEvent(ROOT_TEST_DIR.resolve("myFile.java"), "myFile.java", "someHash", System.currentTimeMillis());
+        IEvent ev9 = new CreateEvent(ROOT_TEST_DIR.resolve("path/to/myFile.java"), "myFile.java", "someHash", System.currentTimeMillis()); // should not be ignored -> glob pattern not crossing dir boundaries
+        IEvent ev10 = new CreateEvent(ROOT_TEST_DIR.resolve("path/to/file_with_underlines.txt"), "file_with_underlines.txt", "someHash", System.currentTimeMillis());
+        IEvent ev11 = new MoveEvent(ROOT_TEST_DIR.resolve("path/to/file.txt"), ROOT_TEST_DIR.resolve("path/to/file.swp"), "file.txt", "someHash", System.currentTimeMillis());
+
+        List<IEvent> eventList = new ArrayList<>();
+        eventList.add(ev1);
+        eventList.add(ev2);
+        eventList.add(ev3);
+        eventList.add(ev4);
+        eventList.add(ev5);
+        eventList.add(ev6);
+        eventList.add(ev7);
+        eventList.add(ev8);
+        eventList.add(ev9);
+        eventList.add(ev10);
+        eventList.add(ev11);
+
+
+        List<IEvent> modifiedEvents = ignorePathsModifier2.modify(eventList);
+
+        assertEquals("to less events are ignored", 3, modifiedEvents.size());
+
+        assertThat("First event is delete event", modifiedEvents.get(0), instanceOf(DeleteEvent.class));
+        assertThat("2nd event is move event", modifiedEvents.get(1), instanceOf(MoveEvent.class));
+        assertThat("3rd event is create event", modifiedEvents.get(2), instanceOf(CreateEvent.class));
     }
 }
